282ba465fd2971ec4565f905e02cf2f6fcd28caf
Matt Dennis <msd7734@gmail.com>
2014-03-12 15:08:15 -0400

    Implement Bulbapedia search

--- QuickDex/BulbapediaReferrer.cs
+++ QuickDex/BulbapediaReferrer.cs
@@ -9,6 +9,7 @@ namespace QuickDex
     class BulbapediaReferrer : ISearchStrategy
     {
         private PokeManager manager;
+        private bool? lastSearchSuccess;
 
         /// <summary>
         /// Construct a SearchStrategy for serebii.com
@@ -17,6 +18,7 @@ namespace QuickDex
         public BulbapediaReferrer(PokeManager manager)
         {
             this.manager = manager;
+            lastSearchSuccess = null;
         }
 
         public string GetName()
@@ -26,19 +28,47 @@ namespace QuickDex
 
         public bool? IsLastSearchSuccess()
         {
-            throw new NotImplementedException();
+            return lastSearchSuccess;
         }
 
         public string GotoPokemonEntry(int dexNum, PokeGeneration gen)
         {
             //can only get pages on BP by pokemon name, so need to perform a lookup 
-            throw new NotImplementedException();
+            string pkmName = manager.GetNameById(dexNum);
+            string paddedDexNum = Util.To3DigitStr(dexNum);
+
+            if (pkmName != null)
+            {
+                string url = "http://bulbapedia.bulbagarden.net/wiki/" + pkmName.ToLower();
+                Process.Start(url);
+                lastSearchSuccess = true;
+                return "Opening " + pkmName + "\'s entry on Bulbapedia.";
+            }
+            else
+            {
+                lastSearchSuccess = false;
+                return "A Pokemon of ID #" + paddedDexNum + " was not found.";
+            }
         }
 
         public string GotoPokemonEntry(string pokemon, PokeGeneration gen)
         {
             //pokemon name can be all lowercase or have first letter capitalized
-            throw new NotImplementedException();
+            int? pkmId = manager.GetIdByName(pokemon);
+
+            if (pkmId != null)
+            {
+                string paddedDexNum = Util.To3DigitStr((int)pkmId);
+                string url = "http://bulbapedia.bulbagarden.net/wiki/" + pokemon.ToLower();
+                Process.Start(url);
+                lastSearchSuccess = true;
+                return "Opening " + pokemon + "\'s entry on Bulbapedia.";
+            }
+            else
+            {
+                lastSearchSuccess = false;
+                return "A Pokemon with the name " + pokemon + " was not found.";
+            }
         }
 
 
