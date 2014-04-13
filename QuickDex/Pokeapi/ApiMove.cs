71dfb4711db0b15b091f3049d54ae94a24441ee7
Matt Dennis <msd7734@gmail.com>
2014-04-13 10:51:25 -0400

    Add ApiMove class

---
+++ QuickDex/Pokeapi/ApiMove.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickDex.Pokeapi
+{
+    public class ApiMove
+    {
+        public int accuracy { get; set; }
+        public string category { get; set; }
+        public DateTime created { get; set; }
+        public string description { get; set; }
+        public int id { get; set; }
+        public DateTime modified { get; set; }
+        public string name { get; set; }
+        public int power { get; set; }
+        public int pp { get; set; }
+        public string resource_uri { get; set; }
+    }
+
+}
